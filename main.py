78765254614592715cd0ba27514235cde58bdfcc
Macaron <72959185+H4011@users.noreply.github.com>
2021-10-16 19:27:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 import os
+import requests
+from bs4 import BeautifulSoup
 
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
+load_url = os.environ["URL"]
+html = requests.get(load_url)
+soup = BeautifulSoup(html.content, "html.parser")
+soup = str(soup).replace('<br>','・')
+soup2 = soup[:-1].split('・')
 
-def main():
-    user_id = os.environ["LINE_MY_USERID"]
-
-    messages = TextSendMessage(text=f"test")
-    line_bot_api.push_message(user_id, messages=messages)
-
-
-if __name__ == "__main__":
-    main()
+for i in soup2:
+    line_bot_api.push_message(i, TextSendMessage(text="test"))
