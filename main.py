6a58c1fa124cd73559b895adb8d635f13fe0ac82
Macaron <72959185+H4011@users.noreply.github.com>
2021-10-16 19:47:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 load_url = os.environ["URL"]
 html = requests.get(load_url)
 soup = BeautifulSoup(html.content, "html.parser")
-soup = str(soup).replace('<br>','・')
+soup = str(soup).replace('<br/>','・')
 soup2 = soup[:-1].split('・')
 
 for i in soup2:
